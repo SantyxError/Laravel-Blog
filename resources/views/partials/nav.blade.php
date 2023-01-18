66d3c6245e2cf79af139b125dd5038e8a95d5d88
Santiago Benito <bmsanti@hotmail.com>
2023-01-18 16:41:29 +0100

    Tema 2 - Ejercicio 3

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -3,10 +3,17 @@
     Trozo de codigo que podemos poner en cualquier otra parte, mediante el comando @section 
 --}}
 
-<nav>
+{{-- <nav class="navbar navbar-dark bg-dark">
     <ul>
         <li><a href="{{route('home')}}">Pagina de inicio</a></li>
         <li><a href="{{route ('listado')}}">Listado de posts</a> </li>
     </ul>
+  </nav> --}}
 
-</nav>
+  <nav class="navbar navbar-inverse">
+    <ul class="nav navbar-nav">
+        <li><a href="{{route('home')}}">Pagina de inicio</a></li>
+        <li><a href="{{route ('listado')}}">Listado de posts</a> </li>
+    </ul>
+    <p class="navbar-text">Some text</p>
+  </nav>
