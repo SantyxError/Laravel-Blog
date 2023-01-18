0bc3cf66ff19f21aabe0e0832c35962e3cb25cbe
Santiago Benito <bmsanti@hotmail.com>
2023-01-18 19:16:58 +0100

    Tema 3 Ejercicio 1

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -3,55 +3,26 @@
     Trozo de codigo que podemos poner en cualquier otra parte, mediante el comando @section 
 --}}
 
-{{-- <nav class="navbar navbar-dark bg-dark">
-    <ul>
-        <li><a href="{{route('home')}}">Pagina de inicio</a></li>
-        <li><a href="{{route ('listado')}}">Listado de posts</a> </li>
-    </ul>
-  </nav> --}}
-
-  {{-- <nav class="navbar navbar-inverse ">
-    <ul class="nav navbar-nav ">
-        <li class="nav-item"><a class="nav-link" href="{{route('home')}}">Pagina de inicio</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{route ('listado')}}">Listado de posts</a> </li>
-    </ul>
-   
-  </nav> --}}
-
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-secondary .text-white">
     <div class="container-fluid">
     <a class="navbar-brand" href="#">Blog</a>
-    {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button> --}}
-   
+
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
+
+          {{--
+             Mediante la interpolacion {{}}, ponemos el comando "route" y entre parentesis, el nombre de la ruta. 
+             Si la ruta viene de un resource, en este caso sera "post.index" 
+          --}}
           <a href="{{route ('home')}}" class="nav-link" >Inicio</a>
         </li>
         <li class="nav-item">
-          <a href="{{route ('listado')}}" class="nav-link">Listado de libros</a>
+          <a href="{{route('posts.index')}}" class="nav-link">Listado de libros</a>
         </li>
       </ul>
     </div>
     </div>
   </nav>
 
-{{-- 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" >Inicio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link">Listado de libros</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav> --}}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 
 
-// Route::view('URL', 'NOMBRE VIEW')->name("NOMBRE COMO QUERAMOS LLAMARLO");
+//Route::view('URL', 'NOMBRE VIEW')->name("NOMBRE COMO QUERAMOS LLAMARLO");
 
 Route::view('/inicio', 'welcome')->name("home");
 
@@ -14,3 +15,10 @@ Route::view('/posts_listado', 'listado')->name('listado');
 //Si queremos pasar una id(o cualquier otro parametro), deberiamos hacerlo así:
 
 Route::view('/posts_ficha/{id}', 'listadoId', ['id' => 'id'])->name('listadoId');
+
+
+
+// Mediante "resource", podemos agrupar diferentes url con sus metodos.
+// En este caso loa grupamos en "posts", y a continuacion añadimos el controlador seguido de ::class
+// Por ultimo, indicamos que solo lea los metodos asignados
+Route::resource('posts', PostController::class)->only(['index', 'show', 'create', 'edit']);
